84afadc88d40fb944327121591747bc10ca1e9e2
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2021-09-04 08:16:36 +0000

    Fixed a bad path in install_nodogsplash.sh

--- scripts/install_nodogsplash.sh
+++ scripts/install_nodogsplash.sh
@@ -41,7 +41,7 @@ configure()
   envsubst < ${HAZ_DIR}/configs/etc/nodogsplash/nodogsplash.conf.tmpl > /etc/nodogsplash/nodogsplash.conf
 
   eval echo "Setting up splash pages..." ${STD_LOG_ARG}
-  cp -r ${HAZ_DIR}/etc/nodogsplash/htdocs/* /etc/nodogsplash/htdocs/
+  cp -r ${HAZ_DIR}/configs/etc/nodogsplash/htdocs/* /etc/nodogsplash/htdocs/
 }
 
 ## Install nodogsplash
